0a65f941dafe604d6f2a3e1f3f9d724554dfbebc
Yassinejabori <74773233+Yassinejabori@users.noreply.github.com>
2021-08-02 10:51:58 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import mediapipe as mp
+import pickle
+import pandas as pd
+import cv2
+
+mp_drawing = mp.solutions.drawing_utils # Drawing helpers
+mp_holistic = mp.solutions.holistic # Mediapipe Solutions
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def extract_keypoints(results):
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([lh, rh])
+
+
+with open('body_language18.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+#model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+
+
+def predict():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+
+        img = cv2.imread(image_path)
+        # Make detections
+        image, results = mediapipe_detection(img, holistic)
+
+
+        # Export coordinates
+        row = list(extract_keypoints(results))
+
+        # Make Detections
+        X = pd.DataFrame([row])
+        body_language_class = model.predict(X)[0]
+        body_language_prob = model.predict_proba(X)[0]
+    
+    return render_template("index.html", prediction = body_language_class)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
